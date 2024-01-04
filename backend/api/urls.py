0e4c13dea85882e375684bf935d19e76096a052d
Sverre Nystad <sverrenystad@gmail.com>
2024-01-04 20:30:07 +0100

    Feat: Add LoginView to api/urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from api.views import hello_world
-from users.views import CreateUserView
+from users.views import CreateUserView, LoginView
 
 urlpatterns = [
     path("hello_word", hello_world),
     path("create-user/", CreateUserView.as_view(), name="create-user"),
+    path("login/", LoginView, name="login"),
 ]
