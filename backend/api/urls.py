f819fd5516a73a37437a57f2385937d419ece407
Sverre Nystad <sverrenystad@gmail.com>
2024-01-04 21:25:56 +0100

    Feat: Update CreateUserView to RegisterUser in urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from api.views import hello_world
-from users.views import CreateUserView, LoginView
+from users.views import LoginView, RegisterUser
 
 urlpatterns = [
     path("hello_word", hello_world),
-    path("create-user/", CreateUserView.as_view(), name="create-user"),
+    path("create-user/", RegisterUser, name="create-user"),
     path("login/", LoginView, name="login"),
 ]
