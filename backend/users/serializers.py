e6a341a82cb77c551588d6866693a0c05a629fa1
Sverre Nystad <sverrenystad@gmail.com>
2024-01-04 22:11:23 +0100

    Refactor: user serializers to include help text

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -17,7 +17,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User.objects.create(
+        user: User = User.objects.create(
             username=validated_data["username"],
             email=validated_data["email"],
             first_name=validated_data.get("first_name", ""),
@@ -30,5 +30,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
 
 class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
+    username = serializers.CharField(help_text="The username of the user.")
+    password = serializers.CharField(
+        write_only=True, help_text="The password for the account."
+    )
