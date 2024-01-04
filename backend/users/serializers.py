3c172ebbac941c9314f88a304456bad188dee242
Sverre Nystad <sverrenystad@gmail.com>
2024-01-04 20:20:40 +0100

    Feat(#23): Add UserSerializer to handle user serialization

---
+++ backend/users/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "created_at",
+            "streak_count",
+            "last_active",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            # Handle password hashing
+            password=validated_data["password"],
+        )
+        return user
