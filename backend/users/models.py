d7c5a5ef19c21278bcd1132660c52cce1794f573
Sverre Nystad <sverrenystad@gmail.com>
2024-01-04 16:55:31 +0100

    Feat: Add User model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+# user model
+class User(models.Model):
+    username = models.CharField(max_length=255)
+
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    streak_count = models.IntegerField(default=0)
+    last_active = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
