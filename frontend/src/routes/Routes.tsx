d118c80e82dd555ae8d0490136d2b7a18c395ac3
Sverre Nystad <sverrenystad@gmail.com>
2024-01-05 00:21:48 +0100

    Feat: Make App component route to page components described in routes.tsx

---
+++ frontend/src/routes/Routes.tsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import routes from "./routesDefinitions.ts";
+
+// Pages
+// import Dashboard from "../pages/dashboard.tsx";
+import Login from "../pages/Login.tsx";
+import Signup from "../pages/Signup.tsx";
+import Page404 from "../pages/Page404.tsx";
+
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
--- frontend/src/routes/routesDefinitions.ts
+++ frontend/src/routes/routesDefinitions.ts
@@ -1,7 +1,5 @@
 const baseAPIUrl: string = "http://127.0.0.1:8000/api";
 
-const routes = {};
-
 const apiRoutes = {
   upload: `${baseAPIUrl}/upload`,
 };
