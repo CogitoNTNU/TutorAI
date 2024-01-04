0fd06f88624bc4221f61ab384e106805534b00f1
Sverre Nystad <sverrenystad@gmail.com>
2024-01-05 00:56:02 +0100

    Feat: Add login and signup routes to apiRoutes

--- frontend/src/routes/routesDefinitions.ts
+++ frontend/src/routes/routesDefinitions.ts
@@ -2,6 +2,8 @@ const baseAPIUrl: string = "http://127.0.0.1:8000/api";
 
 const apiRoutes = {
   upload: `${baseAPIUrl}/upload`,
+  login: `${baseAPIUrl}/login`,
+  signup: `${baseAPIUrl}/signup`,
 };
 
-export { routes, apiRoutes };
+export default apiRoutes;
