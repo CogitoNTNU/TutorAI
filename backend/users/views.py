7f952cbbd138aba86bfdc5e6ab303427f96ca532
Sverre Nystad <sverrenystad@gmail.com>
2024-01-04 21:40:29 +0100

    Docs: Add User Management tag to swagger schema

--- backend/users/views.py
+++ backend/users/views.py
@@ -19,6 +19,7 @@ from .serializers import LoginSerializer, RegisterSerializer
     method="post",
     request_body=LoginSerializer,
     operation_description="Login to the application",
+    tags=["User Management"],
 )
 @api_view(["POST"])
 def LoginView(request):
@@ -36,7 +37,12 @@ def LoginView(request):
         )
 
 
-@swagger_auto_schema(method="post", request_body=RegisterSerializer)
+@swagger_auto_schema(
+    method="post",
+    request_body=RegisterSerializer,
+    operation_description="Register a new user to the application. This will create a new user in the database.",
+    tags=["User Management"],
+)
 @api_view(["POST"])
 def RegisterUser(request):
     serializer = RegisterSerializer(data=request.data)
