8cf50adbfe464eab1f9f84cdccece789a75c7a5d
Sverre Nystad <sverrenystad@gmail.com>
2024-01-05 00:10:03 +0100

    Feat(#23): Create page components

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div>
+        <h1>This is the login page</h1>
+        </div>
+    );
+};
+
+export default Login;
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,9 @@
+const Signup = () => {
+    return (
+        <div>
+            <h1>This is the Signup page</h1>
+        </div>
+    );
+};
+
+export default Signup;
