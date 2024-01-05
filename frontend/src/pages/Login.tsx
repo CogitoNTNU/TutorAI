f6750fafc64c67afccb25a812fa3bbb1819fda85
Sverre Nystad <sverrenystad@gmail.com>
2024-01-05 22:03:58 +0100

    Feat: Add login functionality and navigation to signup page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,63 @@
+import { useState } from "react";
+import postLoginAttempt from "../services/LoginService";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginMessage, setLoginMessage] = useState("");
+
+    const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const user = await postLoginAttempt(username, password)
+        .then((response) => {
+            console.log(response);
+            
+            return response;
+        }
+        ).catch((error) => {
+            console.log(error);
+            setLoginMessage("An error occurred during login. Please try again later.");
+            return error;
+        });
+        if (!!user) {
+            setLoginMessage("Login successful." + user.username);
+        } else {
+            setLoginMessage("Incorrect username or password.");
+        }
+    };
+
+    const navigate = useNavigate();
+
+    const navigateToSignup = () => {
+        navigate('/signup');
+    };
+
     return (
         <div>
-        <h1>This is the login page</h1>
+            <h1>This is the login page</h1>
+            <div>
+                {/* Login with username and password */}
+                <form onSubmit={handleLogin}>
+                    <label htmlFor="username">Username</label>
+                    <input 
+                        type="text" 
+                        id="username" 
+                        value={username} 
+                        onChange={(e) => setUsername(e.target.value)} 
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)} 
+                    />
+                    <button type="submit">Login</button>
+                </form>
+                {loginMessage && <p>{loginMessage}</p>}
+            </div>
+            <button onClick={navigateToSignup}>Go to Signup</button>
         </div>
     );
 };
