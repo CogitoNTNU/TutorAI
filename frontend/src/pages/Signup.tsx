f4aefcb14a26f71b90dc476cc454c0beb159bb5d
Sverre Nystad <sverrenystad@gmail.com>
2024-01-05 22:14:11 +0100

    Feat: Add signup functionality and navigation to login page

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,7 +1,87 @@
+import { useState } from "react";
+import postSignupAttempt from "../services/SignupService";
+import { useNavigate } from "react-router-dom";
+import postLoginAttempt from "../services/LoginService";
+import { LoginResponse } from "../types/User";
+
 const Signup = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [signupMessage, setSignupMessage] = useState("");
+    const [loginCredentials, setLoginCredentials] = useState({} as LoginResponse);
+
+    const handleSignup = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            setSignupMessage("Passwords do not match.");
+            return;
+        }
+
+        await postSignupAttempt(username, password, confirmPassword)
+        .then((response) => {
+            console.log(response);
+            if (response) {
+                // Valid signup 
+                postLoginAttempt(username, password)
+                .then((response) => {
+                    console.log(response);
+                    if (response) {
+                        setLoginCredentials(response);
+                    }
+                }).catch((error) => {
+                    console.log(error);
+                    setSignupMessage("An error occurred during signup. Please try again later.");
+                });
+
+                setSignupMessage("Signup successful. Welcome, " + response.user.username);
+            } else {
+                setSignupMessage("Signup failed. Please try again.");
+            }
+        }).catch((error) => {
+            console.log(error);
+            setSignupMessage("An error occurred during signup. Please try again later.");
+        });
+    };
+
+    const navigate = useNavigate();
+
+    const navigateToLogin = () => {
+        navigate('/login');
+    };
+
     return (
         <div>
-            <h1>This is the Signup page</h1>
+            <h1>This is the signup page</h1>
+            <div>
+                {/* Signup with username, password, and confirm password */}
+                <form onSubmit={handleSignup}>
+                    <label htmlFor="username">Username</label>
+                    <input 
+                        type="text" 
+                        id="username" 
+                        value={username} 
+                        onChange={(e) => setUsername(e.target.value)} 
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)} 
+                    />
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input 
+                        type="password" 
+                        id="confirmPassword" 
+                        value={confirmPassword} 
+                        onChange={(e) => setConfirmPassword(e.target.value)} 
+                    />
+                    <button type="submit">Signup</button>
+                </form>
+                {signupMessage && <p>{signupMessage}</p>}
+            </div>
+            <button onClick={navigateToLogin}>Go to Login</button>
         </div>
     );
 };
