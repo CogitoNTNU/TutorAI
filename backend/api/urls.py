11ce031d060c4ea903fc60a79cb97eb789a99fa5
Sverre Nystad <sverrenystad@gmail.com>
2024-01-05 22:36:24 +0100

    Fea(#10)t: Add upload endpoint that checks for pdf files

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from users.views import LoginView, RegisterUser
+from documents.views import upload_pdf
 
 urlpatterns = [
     path("create-user/", RegisterUser, name="create-user"),
     path("login/", LoginView, name="login"),
+    path("upload/", upload_pdf, name="upload"),
 ]
--- backend/documents/views.py
+++ backend/documents/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(["POST"])
+def upload_pdf(request):
+    if "pdf" in request.FILES:
+        pdf_file = request.FILES["pdf"]
+        print(pdf_file, flush=True)
+        # Process or save pdf_file as needed
+        # ...
+
+        return Response(
+            {"message": "PDF uploaded successfully!"}, status=status.HTTP_201_CREATED
+        )
+    else:
+        return Response(
+            {"message": "No PDF file found in request"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
