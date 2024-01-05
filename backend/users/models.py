890aae944d6c82f93522cdd695e09e01cf122bf6
Sverre Nystad <sverrenystad@gmail.com>
2024-01-05 20:22:08 +0100

    Docs: Add help text to last_active and streak_count fields

--- backend/users/models.py
+++ backend/users/models.py
@@ -11,8 +11,10 @@ class User(AbstractUser):
     username, first_name, last_name, email, password, groups, user_permissions, is_staff, is_active, is_superuser, last_login, date_joined
     """
 
-    last_active = models.DateTimeField(auto_now=True)
-    streak_count = models.IntegerField(default=0)
+    last_active = models.DateTimeField(
+        auto_now=True, help_text="Last time the user fulfilled the daily task"
+    )
+    streak_count = models.IntegerField(default=0, help_text="Current streak count")
 
     def __str__(self):
         return self.username
