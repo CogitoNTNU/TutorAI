df41ea645c7d067870948c010f03f7cfe799713c
Olav Selnes Lorentzen <74178373+olavsl@users.noreply.github.com>
2024-01-14 17:05:02 +0100

    Feat: Update textToFlashcards.py. Co-authored: Sverre Nystad <sverrenystad@gmail.com>

--- backend/flashcards/textToFlashcards.py
+++ backend/flashcards/textToFlashcards.py
@@ -7,7 +7,7 @@ openai.api_key = api_key
 sample_info = "Cristiano Ronaldo dos Santos Aveiro GOIH ComM (Portuguese pronunciation: [kɾiʃˈtjɐnu ʁɔˈnaldu]; born 5 February 1985) is a Portuguese professional footballer who plays as a forward for and captains both Saudi Pro League club Al Nassr and the Portugal national team. Widely regarded as one of the greatest players of all-time, Ronaldo has won five Ballon d'Or awards,[note 3] a record three UEFA Men's Player of the Year Awards, and four European Golden Shoes, the most by a European player."
 
 
-def request_chat_completion(previous_message: dict, role: str = "system", message: str = "", functions: list = []) -> str: 
+def request_chat_completion(previous_message: dict, role: str = "system", message: str = "", functions: list = []) -> list[str]: 
     """
     Returns a response from the OpenAI API
 
@@ -18,7 +18,8 @@ def request_chat_completion(previous_message: dict, role: str = "system", messag
         functions (list, optional): The functions to be used. Defaults to [].
     
     Returns:
-        response (str): The response from the OpenAI API
+        response list[str]: The response from the OpenAI API
+        if empty string, an error has occured
     """
     try:
         if(not (role == "system" or "user" or "assistant")):
@@ -73,12 +74,12 @@ def generate_flashcards(sample_info: str = sample_info) -> str:
     result = result.split('|')
 
     return result
-def parse_flashcard(flashcard: str) -> list[dict[str, str]]:
+def parse_flashcard(flashcards_data: list[str]) -> list[dict[str, str]]:
     """
     Returns a list of dictionaries with the front and back of the flashcard
 
     Args:
-        flashcard (str): The flashcard to be parsed
+        flashcards_data (list[str]): The flashcard to be parsed
 
     Returns:
         list[dict[str, str]]: A list of dictionaries with the front and back of the flashcard
@@ -90,7 +91,7 @@ def parse_flashcard(flashcard: str) -> list[dict[str, str]]:
     parse_flashcard = []
     separator = '-'
     
-    for card in flashcard:
+    for card in flashcards_data:
         card = {
             "front": card.split(separator)[0].strip(),
             "back": card.split(separator)[1].strip()
