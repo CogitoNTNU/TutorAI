e150445d5747b8b9772b683fb548e5450d6faf2e
Krisnol <krisnol@stud.ntnu.no>
2024-01-14 18:04:43 +0100

    test(#8): add testcode for creation and retrivial of cards/cardsets

--- backend/flashcards/tests.py
+++ backend/flashcards/tests.py
@@ -1,11 +1,13 @@
 from django.test import TestCase, Client
+from flashcards.models import Cardset, Flashcard
+from users.models import User
 
 # Create your tests here.
 
 
 base = "/api/"
 
-class FlashcardTest(TestCase):
+class GetFlashcardTest(TestCase):
 
     def setUp(self) -> None:
         self.client = Client()
@@ -23,4 +25,46 @@ class FlashcardTest(TestCase):
         # Check that response items have the correct keys
         self.assertIn("front", response.json()[0])
         self.assertIn("back", response.json()[0])
+        
+class testPersistantFlashcard(TestCase):
+    
+    def setUp(self):
+        # This code will run before each test
+        self.user = User.objects.create_user(username="testuser", password="12345")
+        self.valid_username = "username"
+
+        
+    def tearDown(self):
+        # This code will run after each test
+        self.user.delete()
+
+    def test_persitant_cardset(self):
+        self.assertTrue(Cardset.objects.count() == 0)
+        
+        self.cardset = Cardset.objects.create(name="testcardset", description="testcardset", user=self.user)
+        self.assertTrue(Cardset.objects.count() == 1)
+        
+        self.cardset.delete()
+    
+    def test_persitant_flashcard(self):
+        self.assertTrue(Flashcard.objects.count() == 0)
+        
+        self.cardset = Cardset.objects.create(name="testcardset", description="testcardset", user=self.user)
+        self.card1 = Flashcard.objects.create(front="testfront", back="testback", cardset=self.cardset)
+        self.card2 = Flashcard.objects.create(front="testfront2", back="testback2", cardset=self.cardset)
+        self.assertTrue(Flashcard.objects.count() == 2)
+
+        self.cardset.delete()
+        self.card1.delete()
+        self.card2.delete()
+
+    def test_get_flashcards_from_cardset(self):
+        self.cardset = Cardset.objects.create(name="testcardset", description="testcardset", user=self.user)
+        self.assertTrue(self.cardset.flashcard_set.all().count() == 0)
+
+        self.card1 = Flashcard.objects.create(front="testfront", back="testback", cardset=self.cardset)
+        self.card2 = Flashcard.objects.create(front="testfront2", back="testback2", cardset=self.cardset)
+        self.assertTrue(self.cardset.flashcard_set.all().count() == 2)
+        
+
         
