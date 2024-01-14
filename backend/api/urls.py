98b30ac115a73b57f5191e32b3745c25bc80fd04
Krisnol <krisnol@stud.ntnu.no>
2024-01-14 15:50:36 +0100

    feat: add a view and url for flashcard generating

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from users.views import login, register_user
 from documents.views import upload_pdf
+from flashcards.views import generate_mock_flashcard
 
 urlpatterns = [
     path("create-user/", register_user, name="create-user"),
     path("login/", login, name="login"),
     path("upload/", upload_pdf, name="upload"),
+    path("generate-mock-flashcard/", generate_mock_flashcard, name="generate-mock.flashcards"),
 ]
