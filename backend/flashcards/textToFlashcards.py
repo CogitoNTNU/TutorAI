d39acde71e7bff11014c4b837270fe677953f2fa
Krisnol <krisnol@stud.ntnu.no>
2024-02-29 20:08:06 +0100

    feat: implement req chat completion

--- backend/flashcards/textToFlashcards.py
+++ backend/flashcards/textToFlashcards.py
@@ -4,10 +4,10 @@ from config import Config
 api_key = Config().API_KEY
 
 openai.api_key = api_key
-sample_info = "Cristiano Ronaldo dos Santos Aveiro GOIH ComM (Portuguese pronunciation: [kɾiʃˈtjɐnu ʁɔˈnaldu]; born 5 February 1985) is a Portuguese professional footballer who plays as a forward for and captains both Saudi Pro League club Al Nassr and the Portugal national team. Widely regarded as one of the greatest players of all-time, Ronaldo has won five Ballon d'Or awards,[note 3] a record three UEFA Men's Player of the Year Awards, and four European Golden Shoes, the most by a European player."
+sample_info = "Cristiano Ronaldo dos Santos Aveiro GOIH ComM (Portuguese pronunciation: [kɾiʃˈtjɐnu ʁɔˈnaldu]; born 5 February 1985) is a Portuguese professional footballer who plays as a forward for and captains both Saudi Pro League club Al Nassr and the Portugal national team. Widely regarded as one of the greatest players of all-time, Ronaldo has won five Ballon d'Or awards,[note 3] a record three UEFA Men's Player of the Year Awards, and four European Golden Shoes, the most by a European player. Kaamya does not like coffe. Kristoffer does like coffee. "
 
 
-def request_chat_completion(previous_message: dict, role: str = "system", message: str = "", functions: list = []) -> list[str]: 
+def request_chat_completion(role: str = "system", message: str = "") -> list[str]: 
     """
     Returns a response from the OpenAI API
 
@@ -21,16 +21,28 @@ def request_chat_completion(previous_message: dict, role: str = "system", messag
         response list[str]: The response from the OpenAI API
         if empty string, an error has occured
     """
-    # TODO: Create this function
-    return ""
+    result = ""
+    if message == "":
+        result = "Error: No message provided"
+    else:
+        response = openai.chat.completions.create(
+            model="gpt-4",
+            messages=[
+                {"role": role, "content": message}
+            ]
+        )
+        result = response.choices[0].message.content
+    return result
     
 def generate_template(sample_info: str = sample_info) -> str:
     """
     Returns a template with the correct flashcard and prompt format which can be used to generate flashcards using the sample text
     """
     # TODO: Create this function
+    example = f"Front: Which year was the person born?, Back: 1999 | Front: At what temperture does water boil?, Back: 100 degrees celsius | Front: MAC, Back: Message Authentication Code"
 
-    template = f"Create a set of flashcards from the following text: {sample_info}"
+
+    template = f"Create a set of flashcards using the following format: {example} from the following text: {sample_info}. Use only information from the text to generate the flashcards. Use only the given format"
 
     return template
 
@@ -46,7 +58,8 @@ def generate_flashcards(sample_info: str = sample_info) -> str:
     """
     # TODO: Create this function
     template = generate_template(sample_info)
-    response = request_chat_completion({}, "system", template, [])
+    response = request_chat_completion("system", message=template)
+    response = response.split("|")
     return response
     
 def parse_flashcard(flashcards_data: list[str]) -> list[dict[str, str]]:
