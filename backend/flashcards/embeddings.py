cd4f662e21395dc685c8c174bb3dba363dd6cf65
Parleen Brar <brarparleen88@gmail.com>
2024-03-05 18:29:28 +0100

    embeddings code

---
+++ backend/flashcards/embeddings.py
@@ -0,0 +1,9 @@
+from openai import OpenAI
+client = OpenAI()
+
+def get_embedding(text, model="text-embedding-ada-002"):
+   text = text.replace("\n", " ")
+   return client.embeddings.create(input = [text], model=model).data[0].embedding
+
+print(get_embedding("Hello World"))
+
