f12225e46047c65e30234a9946f859f71ce8f0e9
LockedInTheSkage <LockedInTheSkage@users.noreply.github.com>
2024-03-07 18:50:41 +0100

    Add reader documentation

--- backend/flashcards/text_scraper/text_extractor.py
+++ backend/flashcards/text_scraper/text_extractor.py
@@ -9,3 +9,13 @@ class TextExtractor:
     def extractTextPdf(self, filename):
         self.reader.read(filename)
         self.pages=self.reader.pages
+
+
+
+
+if __name__=="__main__":
+    extr=TextExtractor()
+    extr.extractTextPdf("assets/book-riscv-rev1.pdf")
+    for page in extr.pages:
+        print(page)
+    print(extr.reader.bookname)
--- backend/flashcards/text_scraper/text_reader.py
+++ backend/flashcards/text_scraper/text_reader.py
@@ -2,29 +2,54 @@ import PyPDF2
 
 
 class TextReader:
+    """
+    A class for reading text from PDF files.
+
+    Attributes:
+        pages (list): A list to store text content extracted from each page of the PDF.
+        bookname (str): Name of the PDF file being read.
+    """
 
     def __init__(self) -> None:
-        self.pages=[]
-        self.bookname=""
+        """
+        Initializes the TextReader object with empty lists for pages and an empty string for bookname.
+        """
+        self.pages = []
+        self.bookname = ""
 
     def __str__(self) -> str:
-        return f"{self.bookname}({' '.join(self.pages)})"    
+        """
+        Returns a string representation of the TextReader object.
 
-    def read(self,pdf_file):
+        Returns:
+            str: A string containing the bookname followed by the text from all pages.
+        """
+        return f"{self.bookname}({' '.join(self.pages)})"
+
+    def read(self, pdf_file):
+        """
+        Reads text from the given PDF file.
+
+        Args:
+            pdf_file (str): The path to the PDF file.
+
+        Raises:
+            FileNotFoundError: If the specified PDF file does not exist.
+
+        Notes:
+            This method extracts text from each page of the PDF file and stores it in the 'pages' list.
+            It also extracts the name of the PDF file (without the .pdf extension) and stores it in 'bookname'.
+        """
         # Open the PDF file
         with open(pdf_file, 'rb') as f:
             # Create a PDF reader object
             pdf_reader = PyPDF2.PdfReader(f)
-            
-            self.pages=[]
-            
+
+            self.pages = []
+
             # Iterate through each page of the PDF
             for page_num in range(len(pdf_reader.pages)):
                 # Get the text from the current page
-                page = pdf_reader.pages[page_num] 
+                page = pdf_reader.pages[page_num]
                 self.pages.append(page.extract_text())
-            self.bookname=pdf_file[:pdf_file.find(".pdf")]
-    
-
-
-        
+            self.bookname = pdf_file[:pdf_file.find(".pdf")]
