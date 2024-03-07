9fbc555f19eafa09e791f34fa856d12ec7ab2108
Parleen Brar <brarparleen88@gmail.com>
2024-03-07 18:09:06 +0100

    Feat: Cosine similarity.

--- backend/flashcards/embeddings.py
+++ backend/flashcards/embeddings.py
@@ -1,6 +1,7 @@
 from abc import ABC, ABCMeta, abstractmethod
 import openai
 from config import Config
+import numpy as np
 
 class EmbeddingsInterface(ABC):
     @abstractmethod
@@ -30,9 +31,26 @@ class Embeddings(EmbeddingsInterface):
         )
 
         return response.data[0].embedding
+    
+    def cosine_similarity(embedding1, embedding2):
+        sum_times = 0
+        embedding1_sq = 0
+        embedding2_sq = 0
+        for i in range(len(embedding1)):
+            sum_times += embedding1[i]*embedding2[i]
+            embedding1_sq += embedding1[i]**2
+            embedding2_sq += embedding2[i]**2
+        root_times_emb1_emb2 = np.sqrt(embedding1_sq*embedding2_sq)
+        return sum_times/root_times_emb1_emb2
+
+
 
 if __name__ == "__main__":
     # Example use
     embeddings = Embeddings()
 
-    print(embeddings.get_embedding("Hello world!!"))
+    embedding1 = embeddings.get_embedding("Hello world!!")
+    embedding2 = embeddings.get_embedding("Olav is cool!!")
+    print(embeddings.cosine_similarity(embedding1, embedding2))
+    
+
