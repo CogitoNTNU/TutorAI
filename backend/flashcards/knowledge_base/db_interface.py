1caaa39905404b5f425b93eaabd7d046ac709656
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-03-14 16:22:51 +0100

    refactor: move retriever scripts into folder

--- backend/flashcards/knowledge_base/db_interface.py
+++ backend/flashcards/knowledge_base/db_interface.py
@@ -1,29 +1,29 @@
 from abc import ABC, abstractmethod
+from flashcards.knowledge_base.embeddings import cosine_similarity
 from config import Config
 from pymongo import MongoClient
-from flashcards.embeddings import cosine_similarity
+
 
 class DatabaseInterface(ABC):
-    '''
+    """
     Abstract class for Connecting to a Database
-    '''
+    """
+
     @classmethod
     def __instancecheck__(cls, instance: any) -> bool:
         return cls.__subclasscheck__(type(instance))
-    
+
     @classmethod
     def __subclasscheck__(cls, subclass: any) -> bool:
         return (
-            hasattr(subclass, 'get_curriculum') and
-            callable(subclass.get_curriculum)
+            hasattr(subclass, "get_curriculum") and callable(subclass.get_curriculum)
         ) and (
-            hasattr(subclass, 'post_curriculum') and
-            callable(subclass.post_curriculum)
+            hasattr(subclass, "post_curriculum") and callable(subclass.post_curriculum)
         )
-    
+
     @abstractmethod
-    def get_curriculum(self, query_string: str) -> list[str]:
-        '''
+    def get_curriculum(self, embedding: list[float]) -> list[str]:
+        """
         Get the curriculum from the database
 
         Args:
@@ -31,12 +31,14 @@ class DatabaseInterface(ABC):
 
         Returns:
             list[str]: The curriculum related to the question
-        '''
+        """
         pass
 
     @abstractmethod
-    def post_curriculum(self, curriculum: str, page_num: int, paragraph_num: int) -> bool:
-        '''
+    def post_curriculum(
+        self, curriculum: str, page_num: int, paragraph_num: int, embedding: list[float]
+    ) -> bool:
+        """
         Post the curriculum to the database
 
         Args:
@@ -45,71 +47,79 @@ class DatabaseInterface(ABC):
 
         Returns:
             bool: True if the curriculum was posted, False otherwise
-        '''
+        """
         pass
+
 
 class Database(DatabaseInterface):
     def __init__(self):
         self.client = MongoClient(Config().MONGODB_URI)
-        self.db = self.client['test-curriculum-database']
-        self.collection = self.db['test-curriculum-collection']
+        self.db = self.client["test-curriculum-database"]
+        self.collection = self.db["test-curriculum-collection"]
         self.similarity_threshold = 0.83
 
-    def get_curriculum(self, query_string: list[str], model) -> list[str]:
-        
-        #Create embedding from query
-        S = EmbeddingsInterface(ABC)
-        embedding = S.get_embedding(self, query_string, model)
-
-        #Checking if embedding consists of decimals or "none"
+    def get_curriculum(self, embedding: list[float]) -> list[str]:
+        # Checking if embedding consists of decimals or "none"
         if not embedding:
-            raise ValueError('Embedding cannot be None')
-        
+            raise ValueError("Embedding cannot be None")
+
         # Define the MongoDB query that utilizes the search index "embeddings".
         query = {
             "$vectorSearch": {
                 "index": "embeddings",
                 "path": "embedding",
                 "queryVector": embedding,
-                "numCandidates": 30, # MongoDB suggests using numCandidates=10*limit or numCandidates=20*limit
-                "limit": 3
+                "numCandidates": 30,  # MongoDB suggests using numCandidates=10*limit or numCandidates=20*limit
+                "limit": 3,
             }
         }
-        
+
         # Execute the query
         documents = self.collection.aggregate([query])
 
         if not documents:
-            raise ValueError('No documents found')
-        
+            raise ValueError("No documents found")
+
         # Convert the documents to a list
         documents = list(documents)
 
         # Filter out the documents with low similarity
         for document in documents:
-            if cosine_similarity(embedding, document['embedding']) < self.similarity_threshold:
+            if (
+                cosine_similarity(embedding, document["embedding"])
+                < self.similarity_threshold
+            ):
                 documents.remove(document)
 
         # Return only the text content of the documents
-        documents = [document['text'] for document in documents]
+        documents = [document["text"] for document in documents]
         return documents
 
-    def post_curriculum(self, curriculum: str, page_num: int, paragraph_num: int, embedding: list[float]) -> bool:
+    def post_curriculum(
+        self, curriculum: str, page_num: int, paragraph_num: int, embedding: list[float]
+    ) -> bool:
         if not curriculum:
-            raise ValueError('Curriculum cannot be None')
+            raise ValueError("Curriculum cannot be None")
 
         if not page_num:
-            raise ValueError('Page number cannot be None')
-        
+            raise ValueError("Page number cannot be None")
+
         if not paragraph_num:
-            raise ValueError('Paragraph number cannot be None')
+            raise ValueError("Paragraph number cannot be None")
 
         if not embedding:
-            raise ValueError('Embedding cannot be None')
+            raise ValueError("Embedding cannot be None")
 
         try:
             # Insert the curriculum into the database with metadata
-            self.collection.insert_one({'text': curriculum, 'pageNum': page_num, 'paragraphNum': paragraph_num, 'embedding': embedding})
+            self.collection.insert_one(
+                {
+                    "text": curriculum,
+                    "pageNum": page_num,
+                    "paragraphNum": paragraph_num,
+                    "embedding": embedding,
+                }
+            )
             return True
         except:
             return False
--- backend/flashcards/knowledge_base/embeddings.py
+++ backend/flashcards/knowledge_base/embeddings.py
@@ -3,9 +3,10 @@ import openai
 from config import Config
 import numpy as np
 
+
 class EmbeddingsInterface(ABC):
     @abstractmethod
-    def get_embedding(self, text, model):
+    def get_embedding(self, text: str) -> list[float]:
         """
         Get the embedding of the text using the model
 
@@ -18,27 +19,27 @@ class EmbeddingsInterface(ABC):
         """
         pass
 
+
 class Embeddings(EmbeddingsInterface):
-    def __init__(self):
+    def __init__(self, model_name: str = "text-embedding-ada-002"):
         api_key = Config().API_KEY
         self.client = openai.Client(api_key=api_key)
+        self.model_name = model_name
 
-    def get_embedding(self, text, model="text-embedding-ada-002"):
+    def get_embedding(self, text: str) -> list[float]:
         text = text.replace("\n", " ")
-        response = self.client.embeddings.create(
-            input=text,
-            model=model
-        )
+        response = self.client.embeddings.create(input=text)
         return response.data[0].embedding
-    
+
+
 def cosine_similarity(embedding1, embedding2):
     """
     Calculate the cosine similarity between two embeddings
-    
+
     Args:
         embedding1 (list[float]): The first embedding
         embedding2 (list[float]): The second embedding
-        
+
     Returns:
         float: The cosine similarity between the two embeddings
     """
@@ -46,8 +47,8 @@ def cosine_similarity(embedding1, embedding2):
     embedding1_sq = 0
     embedding2_sq = 0
     for i in range(len(embedding1)):
-        sum_times += embedding1[i]*embedding2[i]
-        embedding1_sq += embedding1[i]**2
-        embedding2_sq += embedding2[i]**2
-    root_times_emb1_emb2 = np.sqrt(embedding1_sq*embedding2_sq)
-    return sum_times/root_times_emb1_emb2
+        sum_times += embedding1[i] * embedding2[i]
+        embedding1_sq += embedding1[i] ** 2
+        embedding2_sq += embedding2[i] ** 2
+    root_times_emb1_emb2 = np.sqrt(embedding1_sq * embedding2_sq)
+    return sum_times / root_times_emb1_emb2
