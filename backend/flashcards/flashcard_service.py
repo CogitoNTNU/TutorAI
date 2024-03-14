8813beaf10c95863eb5c0b94094661afeed6d57f
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-03-14 17:03:22 +0100

    feat: Add flashcard service module

---
+++ backend/flashcards/flashcard_service.py
@@ -0,0 +1,21 @@
+""" The service module contains the business logic of the application. """
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+
+def process_flashcards(uploaded_file: InMemoryUploadedFile) -> list["Flashcard"]:
+    """
+    Process the files and return the flashcards.
+    """
+    print("[INFO] Processing file", flush=True)
+
+    # Extract text from the uploaded file
+    # TODO: Use the scraper to extract the text from the uploaded file
+
+    # Post the text into knowledge base
+    # TODO: Use the rag service to post the text into the knowledge base
+
+    # Generate flashcards from the text
+    # TODO: use the FlashcardGenerator to generate flashcards from the text
+
+    # Format the texts into flashcards
