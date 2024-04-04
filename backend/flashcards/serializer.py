6e9837ca7a9a18ec13a2246da475dc13d4f6ffda
Krisnol <krisnol@stud.ntnu.no>
2024-04-04 19:43:04 +0200

    fix: correct curriculem serializer. Co-authored-by: Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>

--- backend/flashcards/serializer.py
+++ backend/flashcards/serializer.py
@@ -3,12 +3,5 @@ from rest_framework import serializers
 
 
 class CurriculumSerializer(serializers.Serializer):
-    curriculum = serializers.ListField(
-        child=serializers.FileField(allow_empty_file=False, use_url=False),
-    )
-
-    def validate_curriculum(self, value):
-        # Check minimum number of files
-        if len(value) < 1:
-            raise serializers.ValidationError("At least one file must be uploaded.")
-        return value
+    
+    serializers.FileField(allow_empty_file=False, use_url=False)
