bea32fd59570736a54e1e4a028da5f1dd4801b25
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-04-04 18:42:11 +0200

    feat: remove duplicate api url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     # path("create-user/", register_user, name="create-user"),
     # path("login/", login, name="login"),
     path("health-check/", health_check, name="Health_check"),
-    path("generate-mock-flashcard/", generate_mock_flashcard, name="generate-mock-flashcards"),
     path("create-flashcards/", create_flashcards, name="create-flashcards"),
     path("generate-mock-flashcard/", generate_mock_flashcard, name="generate-mock-flashcard"),
 ]
