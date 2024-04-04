3627d3c3294bfa5a25331f957ede6113105c82db
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-04-04 17:58:50 +0200

    feat: add health_check view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-# from users.views import login, register_user
+from .views import health_check
 from flashcards.views import create_flashcards, generate_mock_flashcard
 
 urlpatterns = [
     # path("create-user/", register_user, name="create-user"),
     # path("login/", login, name="login"),
+    path("health-check/", health_check, name="Health_check"),
     path("generate-mock-flashcard/", generate_mock_flashcard, name="generate-mock-flashcards"),
     path("create-flashcards/", create_flashcards, name="create-flashcards"),
     path("generate-mock-flashcard/", generate_mock_flashcard, name="generate-mock-flashcard"),
