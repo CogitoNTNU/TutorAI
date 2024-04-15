9f33a472c0fafad213be65766249a32b8fa3a569
SverreNystad <sverrenystad@gmail.com>
2024-04-15 14:46:58 +0200

    refactor: make method signatures simpler in rag service

--- backend/flashcards/rag_service.py
+++ backend/flashcards/rag_service.py
@@ -6,17 +6,16 @@ from flashcards.knowledge_base.factory import create_database
 from flashcards.knowledge_base.factory import create_embeddings_model
 
 
+db: DatabaseInterface = create_database()
+embeddings: EmbeddingsInterface = create_embeddings_model()
 
-def get_context(
-    query: str, db: DatabaseInterface, embeddings: EmbeddingsInterface
-) -> list[str]:
+
+def get_context(query: str) -> list[str]:
     """
     Get the context of the query
 
     Args:
         query (str): The query to get the context of
-        db (DatabaseInterface): The database to be used
-        embeddings (EmbeddingsInterface): The embeddings to be used
 
     Returns:
         list[str]: The context of the query
@@ -38,14 +37,10 @@ def post_context(
         context (str): The context to be posted
         page_num (int): The page number of the context
         paragraph_num (int): The paragraph number of the context
-        db (DatabaseInterface): The database to be used
-        embeddings (EmbeddingsInterface): The embeddings to be used
 
     Returns:
         bool: True if the context was posted, False otherwise
     """
-    db: DatabaseInterface = create_database()
-    embeddings: EmbeddingsInterface = create_embeddings_model()
 
     embedding = embeddings.get_embedding(context)
     return db.post_curriculum(context, page_num, pdf_name, embedding)
