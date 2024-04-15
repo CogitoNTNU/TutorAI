5beae3941395ae72df36a984a7a204f4acbc23e9
SverreNystad <sverrenystad@gmail.com>
2024-04-15 13:30:26 +0200

    refactor: make flashcard processing

--- backend/flashcards/flashcard_service.py
+++ backend/flashcards/flashcard_service.py
@@ -4,7 +4,7 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 from flashcards.rag_service import post_context
 from flashcards.text_to_flashcards import Flashcard, generate_flashcards
 from flashcards.text_scraper.text_extractor import TextExtractor
-from flashcards.text_scraper.post_processing import PostProcessor, Data
+from flashcards.text_scraper.post_processing import Page
 
 
 def process_flashcards(uploaded_file: InMemoryUploadedFile) -> list[Flashcard]:
@@ -15,19 +15,16 @@ def process_flashcards(uploaded_file: InMemoryUploadedFile) -> list[Flashcard]:
 
     # Extract text from the uploaded file
     extractor = TextExtractor()
-    paragraph_data: list[Data] = extractor.extractParagraphs(uploaded_file)
-    for index, page in enumerate(paragraph_data):
-        context: str = page.text
-        page_num: int = page.page_num
-        pdf_name: str = page.pdf_name
-        if page_num == None:
-            raise Exception("Page number is null")
-        if context == None:
-            continue
-        if pdf_name == None:
-            raise Exception("PDF name is null")
-        context_posted: bool = post_context(context, page_num, index)
+    pages: list[Page] = extractor.extractData(uploaded_file)
 
-    print("[INFO] Generating flashcards", flush=True)
-    flashcards = generate_flashcards(context)
+    flashcards: list[Flashcard] = []
+    for index, page in enumerate(pages):
+        # TODO: Parallelize api calls
+        print("[INFO] Generating flashcards", flush=True)
+        flashcards_from_page = generate_flashcards(page.text)
+        flashcards.extend(flashcards_from_page)
+        # Save content
+        print(f"New flashcards {flashcards_from_page}")
+        context_posted: bool = post_context(page.text, page.page_num, index)
+
     return flashcards
