ad1d8e88d08db6d7094efc9fdd91826cc2348fbc
SverreNystad <sverrenystad@gmail.com>
2024-04-15 15:28:39 +0200

    fix: syntax error in manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -6,10 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorai.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tutorai.settings")
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc
+    except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
@@ -18,5 +18,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
