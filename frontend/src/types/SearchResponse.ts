9bc071925ffb06dd4c36801e92b7307b05b82f79
SverreNystad <sverrenystad@gmail.com>
2024-04-29 21:43:33 +0200

    feat: Update SearchResponse.ts with new interfaces and types

--- frontend/src/types/SearchResponse.ts
+++ frontend/src/types/SearchResponse.ts
@@ -1,7 +1,16 @@
-import { SearchProps } from '../pages/InformationSearch'
+import { SearchProps } from "../pages/InformationSearch";
 
-type SearchResponse = {
-    data: SearchProps;
+export interface ChatMessageData {
+  role: string;
+  content: string;
+}
+
+export interface ChatData {
+  documents: string[]; // The names of the documents
+  user_question: string; // The user question
+  chat_history?: ChatMessageData[]; // The chat history, optional
+}
+
+export type SearchResponse = {
+  data: SearchProps;
 };
-
-export default SearchResponse;
