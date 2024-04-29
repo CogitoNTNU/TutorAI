b3a9686a329cbc32db9e7ef239e6924b67908dba
SverreNystad <sverrenystad@gmail.com>
2024-04-29 22:18:59 +0200

    feat: add createQuiz and gradeQuizAnswer service functions

--- frontend/src/services/QuizService.ts
+++ frontend/src/services/QuizService.ts
@@ -1,39 +1,48 @@
 import axios from "axios";
 import apiRoutes from "../routes/routesDefinitions";
+import {
+  DocumentData,
+  GradedQuiz,
+  Quiz,
+  QuizStudentAnswerData,
+} from "../types/Quiz";
 
-interface Quiz {
-  document: string;
-  start: number;
-  end: number;
-  content: QuestionAnswers[];
-}
-
-interface QuestionAnswers {
-  question: string;
-  answer: string;
-}
-
-const QuizService = async (
-  document_name: string,
-  start_index: number,
-  end_index: number
-): Promise<Quiz> => {
-  const quizRequest = {
-    document: document_name,
-    start: start_index,
-    end: end_index,
-  };
-
-  const response = await axios
-    .post(apiRoutes.quiz, quizRequest)
-    .then((res) => {
-      return res;
-    })
-    .catch((err) => {
-      return err;
-    });
-
-  return response;
+/**
+ * Service to create a quiz.
+ * @param data - The document data to generate the quiz.
+ */
+export const createQuiz = async (data: DocumentData): Promise<Quiz> => {
+  try {
+    const response = await axios.post(apiRoutes.quiz, data);
+    if (response.status === 200) {
+      console.log("Quiz created successfully", response.data);
+      return response.data;
+    } else {
+      throw new Error("Failed to create quiz");
+    }
+  } catch (error: any) {
+    console.error("Error creating quiz:", error.message || error);
+    throw error;
+  }
 };
 
-export default QuizService;
+/**
+ * Service to grade a quiz.
+ * @param data - The answers data to grade the quiz.
+ */
+export const gradeQuizAnswer = async (
+  data: QuizStudentAnswerData
+): Promise<GradedQuiz> => {
+  try {
+    const response = await axios.post(apiRoutes.gradeQuizAnswer, data);
+    if (response.status === 200) {
+      console.log("Quiz graded successfully", response.data);
+      return response.data;
+    } else {
+      throw new Error("Failed to grade quiz");
+    }
+  } catch (error: any) {
+    console.error("Error grading quiz:", error.message || error);
+    throw error;
+  }
+};
