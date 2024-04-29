78c49865d38f6df215f5e2fd9328bc556abedb08
SverreNystad <sverrenystad@gmail.com>
2024-04-29 22:11:33 +0200

    feat: add quiz types

--- frontend/src/types/Quiz.ts
+++ frontend/src/types/Quiz.ts
@@ -1,11 +1,29 @@
-interface QuestionAnswer {
-  question: string;
-  answer: string;
-}
-
-interface Quiz {
+export interface DocumentData {
   document: string;
   start: number;
   end: number;
-  questions: QuestionAnswer[];
+  learningGoals?: string[];
+}
+
+export interface QuizStudentAnswerData {
+  questions: string[];
+  correct_answers: string[];
+  student_answers: string[];
+}
+
+export interface GradedQuiz {
+  answers_was_correct: boolean[];
+  feedback: string[];
+}
+
+export interface Quiz {
+  document: string;
+  start: number;
+  end: number;
+  questions: Question[];
+}
+
+export interface Question {
+  question: string;
+  answer: string;
 }
