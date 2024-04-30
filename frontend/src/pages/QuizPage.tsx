83dcbc37a5739c29aeb1c29ac220adc8f3ea62cc
Olav <olavslo@stud.ntnu.no>
2024-04-30 15:13:11 +0200

    style

--- frontend/src/pages/QuizPage.tsx
+++ frontend/src/pages/QuizPage.tsx
@@ -16,7 +16,7 @@ const QuizPage: React.FC = () => {
     const [activeQuiz, setActiveQuiz] = useState<Quiz | null>(null);
 
     return (
-        <div className="bg-blue-100 relative flex flex-grow w-full flex flex-col ">
+        <div className="bg-blue-50 relative flex flex-grow w-full flex flex-col ">
             <QuizContext.Provider value={{activeQuiz, setActiveQuiz}}>
                 <SideBar />
                 <div className="absolute left-1/2 transform -translate-x-1/2 w-2/5 h-full'">
