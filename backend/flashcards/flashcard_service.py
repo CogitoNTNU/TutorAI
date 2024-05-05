31194e0dbe7318c0b500582b0ba2951a66dc4e19
SverreNystad <sverrenystad@gmail.com>
2024-05-05 14:56:00 +0200

    refactor: Update generate_compendium_template function to be private and add deliminator to prompt

--- backend/flashcards/flashcard_service.py
+++ backend/flashcards/flashcard_service.py
@@ -138,19 +138,6 @@ def grade_quiz(
     return graded_quiz
 
 
-def generate_compendium_template(text: str) -> tuple[str, str]:
-    """
-    Generate a compendium template for the text
-    """
-
-    key_concepts_format = "France: A large country in westeren Europe | Java Virtual Machine: A microarchitecture that the Java programming language uses | Deforestation: The process in which a forest is destroyed by humans"
-    key_concepts_template = f"Generate a list of key concepts from the given text{text}. Use only the most important concepts. Do not include any unnecessary information. Use only the information that is in the text. Use the following format: {key_concepts_format}"
-
-    summary_template = f"Generate a summary of the given text{text}. Use only the most important information. Do not include any unnecessary information. Use only the information that is in the text."
-
-    return key_concepts_template, summary_template
-
-
 def generate_compendium(document_name: str, start: int, end: int) -> Compendium:
     """
     Generates a compendium for the document
@@ -167,7 +154,7 @@ def generate_compendium(document_name: str, start: int, end: int) -> Compendium:
         # Extract the key concepts and summaries from the page
         # Append the key concepts and summaries to the lists
 
-        concept_template, summary_template = generate_compendium_template(page.text)
+        concept_template, summary_template = _generate_compendium_template(page.text)
         concept = OpenAIFlashcardGenerator.request_chat_completion(
             "system", message=concept_template
         )
@@ -179,3 +166,16 @@ def generate_compendium(document_name: str, start: int, end: int) -> Compendium:
 
     compendium = Compendium(document_name, start, end, key_concepts, summaries)
     return compendium
+
+
+def _generate_compendium_template(text: str) -> tuple[str, str]:
+    """
+    Generate a compendium template for the text
+    """
+
+    key_concepts_format = "France: A large country in westeren Europe | Java Virtual Machine: A microarchitecture that the Java programming language uses | Deforestation: The process in which a forest is destroyed by humans"
+    key_concepts_template = f"Generate a list of key concepts from the given text: '''{text}'''. Use only the most important concepts. Do not include any unnecessary information. Use only the information that is in the text. Use the following format: {key_concepts_format}"
+
+    summary_template = f"Generate a summary of the given text: '''{text}'''. Use only the most important information. Do not include any unnecessary information. Use only the information that is in the text."
+
+    return key_concepts_template, summary_template
