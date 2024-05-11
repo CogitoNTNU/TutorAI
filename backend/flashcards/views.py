1a8ae9cc349c443831b34ed29534a4f1616326fd
SverreNystad <sverrenystad@gmail.com>
2024-05-11 19:47:27 +0200

    fix: Add REST framework status module to views.py

--- backend/flashcards/views.py
+++ backend/flashcards/views.py
@@ -2,6 +2,7 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
+from rest_framework import status
 
 from flashcards.learning_resources import Flashcard, RagAnswer
 from flashcards.flashcard_service import (
